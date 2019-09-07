6ba9520fb98cab23f454f8894a2fa9ebd1180e60
Charles Taylor <charlestaylor95@gmail.com>
2019-09-07 02:10:25 -0400

    Work

--- src/sudoku_solver/project_euler.clj
+++ src/sudoku_solver/project_euler.clj
@@ -7,7 +7,15 @@
 
 (defn indexed
   [coll]
-  (map-indexed #([%1 %2]) coll))
+  (map-indexed (fn [k v] [k v]) coll))
+
+(defn char->digit
+  "Parse character to digit"
+  [char]
+  (-> 
+   char
+   int
+   (- 48)))
 
 (defn soln
   []
@@ -16,23 +24,25 @@
    slurp
    split-lines
    (partition 10)
-   (map 
+   (map
     (fn [lines] 
       (->>
        lines
        (drop 1)
        (fn [board] 
          (for [[i row] (indexed board)
-               [j value] (indexed row)
+               [j char] (indexed row)
+               :let [value (char->digit char)]
                :when (> value 0)]
-           ($= [:cell i j] value)))
-       (concat sudoku-model)
-       solution
-       (fn
-           ; Upper left corner 3 digit number
-         [{hun [:cell 0 0] ten [:cell 0 1] one [:cell 0 2]}] 
-           ; Convert digits to number using Horner's rule 
-         (+ one (* 10 (+ ten (* 10 hun))))))))
-   (reduce +)))
+           (do (println value) 
+           ($= [:cell i j] value))))
+       (concat sudoku-model))))))
+  ;      solution
+  ;      (fn
+  ;          ; Upper left corner 3 digit number
+  ;        [{hun [:cell 0 0] ten [:cell 0 1] one [:cell 0 2]}] 
+  ;          ; Convert digits to number using Horner's rule 
+  ;        (+ one (* 10 (+ ten (* 10 hun))))))))
+  ;  (reduce +)))
 
 (soln)
