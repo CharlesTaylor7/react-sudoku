283eb0452c34a16a3706a6e8584b33ab2517bdb0
Charles Taylor <charlestaylor95@gmail.com>
2019-09-09 20:32:50 -0400

    Updates

---
+++ precache-manifest.874fb1bf6973e87acda730869e8057a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5b6ffa0ed499de5d409c6a421dadd4",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "19fe482abddab7705906",
+    "url": "/react-sudoku/static/css/main.3b6148e6.chunk.css"
+  },
+  {
+    "revision": "824b6e60b0a44fe76ab8",
+    "url": "/react-sudoku/static/js/2.1800c1e8.chunk.js"
+  },
+  {
+    "revision": "19fe482abddab7705906",
+    "url": "/react-sudoku/static/js/main.e7e0980a.chunk.js"
+  },
+  {
+    "revision": "58b35336b640bd8a44b2",
+    "url": "/react-sudoku/static/js/runtime~main.c4e9c9bd.js"
+  }
+]);
